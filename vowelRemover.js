1509c8c406d366eb2d47f43c744a326b52d6c0a9
bubinbei <bubinbei@yandex.ru>
2024-03-19 16:10:23 +0400

    shortcut

---
+++ vowelRemover.js
@@ -0,0 +1,5 @@
+// Create a function called shortcut to remove the lowercase vowels (a, e, i, o, u ) in a given string.
+
+// "hello"     -->  "hll"
+
+const shortcut = s => s.replace(/[aeiou]/gi, '')
