6ef315c7116e6a13ddcaa7230e4ed4a58a1be6af
bubinbei <bubinbei@yandex.ru>
2024-03-31 16:57:21 +0400

    square

--- reverseSeq.ts
+++ reverseSeq.ts
@@ -2,4 +2,5 @@
 
 // Example : n=5 --> [5,4,3,2,1]
 
-export const reverseSeq=(n: number):number[]=>Array.from({length: n},(_,i)=>i+1).reverse()
+export const reverseSeq   = (n: number): number[] => Array.from({length:n},(_,i)=>i+1).reverse()
+export const reverseSeq_2 = (n: number): number[] => [...Array(n+1).keys()].slice(1,n+1).reverse()
