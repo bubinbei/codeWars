432a80d52388b0567a99ea78762735c3ee4f8129
bubinbei <bubinbei@yandex.ru>
2024-04-22 09:27:22 +0400

    findEvenIndex.v2

--- findEvenIndex.js
+++ findEvenIndex.js
@@ -1,5 +1,5 @@
 const findEvenIndex = (arr,t1 = 0,t2 = 0) => {
     t1 = arr.map((e,i,a) => t1=e+t1 )
     t2 = arr.reverse().map((e,i,a) => t2=e+t2 ).reverse()
-    return [t1,t2,t1.findIndex((e,i)=>t1[i-1]===t2[i+1]),t3]
+    return t1.findIndex((e,i)=>t1[i-1]===t2[i+1])
   }
