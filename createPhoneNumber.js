82b4527786312cead4ec6fc2ae50f6fa5f20f284
bubinbei <bubinbei@yandex.ru>
2024-04-22 09:25:03 +0400

    createPhoneNumber

---
+++ createPhoneNumber.js
@@ -0,0 +1,3 @@
+// Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
+
+const createPhoneNumber=(n)=> '('+n[0]+n[1]+n[2]+') '+n[3]+n[4]+n[5]+'-'+n[6]+n[7]+n[8]+n[9]
