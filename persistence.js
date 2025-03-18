86d8b81aa4976a2409de0d81dc2b923f9235be0d
bubinbei <bubinbei@yandex.ru>
2025-03-18 05:02:48 +0400

    persistence refactoring v2

--- persistence.js
+++ persistence.js
@@ -17,12 +17,18 @@
 
 // тут немного подумал, ни к чему плодить функции, можно и одной обойтись
 
-export function persistence(num, a = 0) {
-    if(num>9) {
-      num = [...''+num].reduce((a, b)=> {return a * b},1)
-      a++
-      }else{
-        return a
-      }
-return persistence(num, a)
-}
+// export function persistence(num, a = 0) {
+//     if(num>9) {
+//       num = [...''+num].reduce((a, b)=> {return a * b},1)
+//       a++
+//       }else{
+//         return a
+//       }
+// return persistence(num, a)
+// }
+
+// тут я совсем не думал, и долго не мог понять почему тернарник не хочет жрать return
+
+export const persistence = (num, a = 0) => num>9 ? persistence([...''+num].reduce((a, b)=> a * b, 1), a+1) : a
+
+// всё, я пошёл к реке
