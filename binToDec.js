063d9602324d648e983a2c96904f1fd65e7aebc1
bubinbei <bubinbei@yandex.ru>
2025-05-13 07:51:16 +0400

    + binToDec

--- binToDec.js
+++ binToDec.js
@@ -1,2 +1,3 @@
 // export const binToDec = (bin) => parseInt(bin, 2);
-export const binToDec = (bin) => Number('0b'+bin);
+// export const binToDec = (bin) => Number('0b'+bin);
+export const binToDec = b => +('0b'+b);
